79db0296ca422987afa3a31a6421e86b7cb4a7a7
XxAzeSxX <60826595+XxAzeSxX@users.noreply.github.com>
2020-02-08 22:49:25 +0100

    conv to py3
    title says it all thy to d4v1nc13

--- exe2shell.py
+++ exe2shell.py
@@ -1,20 +1,20 @@
 import sys, os
 
 if len(sys.argv) < 2:
-    print 'usage: ' + sys.argv[0] + ' file.exe\n'
+    print('usage: ' + sys.argv[0] + ' file.exe\n')
     sys.exit(0)
 
 shellcode = ''
-bytes = 0
+bytes_ = 0
 
 for b in open(sys.argv[1], 'rb').read():
     shellcode += '\\x' + b.encode('hex')
-    bytes += 1
+    bytes_ += 1
 
-print 'Number of bytes for file ' + sys.argv[1] + ': ' + str(bytes) + '\n'
+print ('Number of bytes for file ' + sys.argv[1] + ': ' + str(bytes_) + '\n')
 
 fp=open("shell.txt", "w")
 fp.write(shellcode)
 fp.close()
 
-print "Done!"
+print("Done!")
