22e41e4b43786ccb3c3b56cdf23340f007468104
XxAzeSxX <60826595+XxAzeSxX@users.noreply.github.com>
2020-02-08 23:03:35 +0100

    py3 / ui

---
+++ e2s_py3.py
@@ -0,0 +1,22 @@
+import sys, os
+
+if len(sys.argv) < 2:
+    print('usage: ' + sys.argv[0] + ' file.exe\n')
+    sys.exit(0)
+
+shellcode = ''
+bytes_ = 0
+
+for b in open(sys.argv[1], 'rb').read():
+    print('\\' + hex(b), end="\r", flush=True)
+    shellcode += '\\' + hex(b)
+    #print(shellcode,flush=True)
+    bytes_ += 1
+
+print ('Number of bytes for file ' + sys.argv[1] + ': ' + str(bytes_) + '\n')
+
+fp=open(sys.argv[0].split("\\")[len(sys.argv[0].split("\\") - 1)] + ".shell.txt", "w")
+fp.write(shellcode)
+fp.close()
+
+print("Done!")
